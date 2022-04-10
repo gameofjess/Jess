df568631418317c00422fad1d02c7839f7087d3a
Uggah <luccagreschner@gmail.com>
2022-04-10 11:11:56 +0200

    Update module-info.java
    Add Java-Websocket to dependencies
    Add log4j to dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module SE2StartupProject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires Java.WebSocket;
+    requires org.apache.logging.log4j;
 
     opens org.example.javachess.mainpackage;
 }
