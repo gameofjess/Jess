3257237e665bb7c348785be1ca4999318dd76487
Uggah <luccagreschner@gmail.com>
2022-04-10 15:39:13 +0200

    Fix packaged jar not executable

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module SE2StartupProject {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires Java.WebSocket;
-    requires org.apache.logging.log4j;
-
-    opens org.example.javachess.mainpackage;
-}
