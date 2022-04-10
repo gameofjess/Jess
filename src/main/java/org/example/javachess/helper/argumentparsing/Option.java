528c8a51b395063b33dfbe167af3a5b6859c331a
Uggah <luccagreschner@gmail.com>
2022-04-10 15:43:52 +0200

    Add ArgumentParser

---
+++ src/main/java/org/example/javachess/helper/argumentparsing/Option.java
@@ -0,0 +1,93 @@
+package org.example.javachess.helper.argumentparsing;
+
+/**
+ * Represents all possible command line options for JavaChess.
+ */
+
+public enum Option {
+
+    /**
+     * Just the dedicated server should be started.
+     */
+    dedicatedServer('s', "server"),
+    /**
+     * A different host should be used.
+     */
+    host('H', "host"),
+    /**
+     * A different port shall be used.
+     */
+    port('p', "port");
+
+    private final char shortAlias;
+    private final String longAlias;
+    private String value;
+
+    Option(Character shortAlias, String longAlias){
+        this.shortAlias = shortAlias;
+        this.longAlias = longAlias;
+    }
+
+    /**
+     * Setter for an options value. E.g. "--port 1234"
+     * @param value value to be set.
+     */
+    public void setValue(String value){
+        this.value = value;
+    }
+
+    /**
+     * Getter for an options value.
+     * @return value
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * Getter for shortAlias
+     * @return the short alias for the specific option object.
+     */
+    public char getShortAlias(){
+        return this.shortAlias;
+    }
+
+    /**
+     * Getter for longAlias
+     * @return the long alias for the specific option object.
+     */
+    public String getLongAlias(){
+        return this.longAlias;
+    }
+
+    /**
+     * This method can be used to get a specific Option object by its short alias.
+     * @param shortAlias short alias of object to be returned.
+     * @return Option object that is represented by given short alias or null if such an object does not exist.
+     */
+
+    public static Option getByShortAlias(char shortAlias){
+        for(Option opt : values()){
+            if( opt.getShortAlias() == shortAlias){
+                return opt;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * This method can be used to get a specific Option object by its long alias.
+     * @param longAlias long alias of object to be returned.
+     * @return Option object that is represented by given long alias or null if such an object does not exist.
+     */
+
+    public static Option getByLongAlias(String longAlias){
+        for(Option opt : values()){
+            if(opt.getLongAlias().equals(longAlias)){
+                return opt;
+            }
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/org/example/javachess/helper/exceptions/InvalidOptionException.java
@@ -0,0 +1,10 @@
+package org.example.javachess.helper.exceptions;
+
+/**
+ * Invalid argument / option
+ */
+public class InvalidOptionException extends Exception{
+    public InvalidOptionException(){
+        super("An invalid Option was given!");
+    }
+}
