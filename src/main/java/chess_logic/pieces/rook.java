5772dda6d0ffa0fbeef0154c972edad1e4b7906d
Kay <kay.knoepfle@gmx.de>
2022-04-11 18:48:39 +0200

    added rook moves, edited .gitignore

--- src/main/java/chess_logic/pieces/rook.java
+++ src/main/java/chess_logic/pieces/rook.java
@@ -1,5 +1,9 @@
 package chess_logic.pieces;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import chess_logic.board;
 import chess_logic.move;
 
 public class rook extends piece {
@@ -9,7 +13,48 @@ public class rook extends piece {
 
 	@Override
 	public move[] getMoves() {
-		// TODO Auto-generated method stub
-		return null;
+		List<move> moves = new ArrayList<move>();
+
+		//rechts
+		for (int i = position[0]+1; i < 8; i++) {
+			piece test_position = board.getPosition(i, position[1]);
+			if (test_position != null && test_position.isWhite == isWhite) {
+				break;
+			}
+			if (test_position == null || test_position.isWhite != isWhite) {
+				moves.add(new move(i, position[1], test_position));
+			}
+		}
+		//links
+		for (int i = position[0]-1; i >= 0; i--) {
+			piece test_position = board.getPosition(i, position[1]);
+			if (test_position != null && test_position.isWhite == isWhite) {
+				break;
+			}
+			if (test_position == null || test_position.isWhite != isWhite) {
+				moves.add(new move(i, position[1], test_position));
+			}
+		}
+		//hoch
+		for (int i = position[1]; i < 8; i++) {
+			piece test_position = board.getPosition(position[0], i);
+			if (test_position != null && test_position.isWhite == isWhite) {
+				break;
+			}
+			if (test_position == null || test_position.isWhite != isWhite) {
+				moves.add(new move(position[0], i, test_position));
+			}
+		}
+		//runter
+		for (int i = position[1]; i >= 0; i--) {
+			piece test_position = board.getPosition(position[0], i);
+			if (test_position != null && test_position.isWhite == isWhite) {
+				break;
+			}
+			if (test_position == null || test_position.isWhite != isWhite) {
+				moves.add(new move(position[0], i, test_position));
+			}
+		}
+		return moves.toArray(new move[moves.size()]);
 	}
 }
