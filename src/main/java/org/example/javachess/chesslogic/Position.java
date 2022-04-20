8014218e834d6c658202682239213e0b5e16d1e4
Kay <kay.knoepfle@gmx.de>
2022-04-20 18:00:22 +0200

    formatting

--- src/main/java/org/example/javachess/chesslogic/Position.java
+++ src/main/java/org/example/javachess/chesslogic/Position.java
@@ -2,26 +2,28 @@ package org.example.javachess.chesslogic;
 
 public class Position {
 	private final int x;
-    private final int y;
+	private final int y;
 
-    public Position(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+	public Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Position)) return false;
-        Position position = (Position) o;
-        return x == position.x && y == position.y;
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof Position))
+			return false;
+		Position position = (Position) o;
+		return x == position.x && y == position.y;
+	}
 
-    @Override
-    public int hashCode() {
-        int result = x;
-        result = 64 * result + y;
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		int result = x;
+		result = 64 * result + y;
+		return result;
+	}
 
 }
