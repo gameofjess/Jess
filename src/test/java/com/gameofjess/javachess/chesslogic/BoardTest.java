9e3a085cec73f5c4951e4d6af771a576ccf1a6cc
Kay <kay.knoepfle@gmx.de>
2022-07-09 15:13:41 +0200

    Update BoardTest.java

--- src/test/java/com/gameofjess/javachess/chesslogic/BoardTest.java
+++ src/test/java/com/gameofjess/javachess/chesslogic/BoardTest.java
@@ -1,21 +1,69 @@
 package com.gameofjess.javachess.chesslogic;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.gameofjess.javachess.chesslogic.pieces.Piece;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 class BoardTest {
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void playtest(){
+        assertDoesNotThrow(() -> {
+            Board board = new Board();
+            for (int i = 0; i < 50; i++) {
+                List<Piece> pieces = new ArrayList<Piece>(board.getBoardMap().values());
+                Piece piece = pieces.get(i% pieces.size());
+                if (i%2 == 0 && piece.isWhite() || i%2 == 1 && !piece.isWhite()){
+                    Move[] moves = piece.getMoves();
+                    if(moves.length > 0){
+                        Move move = moves[i % moves.length];
+                        piece.makeMove(move);
+                    }
+                }
+            }
+        });
     }
 
     @Test
-    void capture() {
-    }
-
-    @Test
-    void isMoveValid() {
+    void initialization(){
+        Board board = new Board();
+        assertEquals(board.toString(),
+                "(0,0) |  null  \n" +
+                "(1,0) |  n  \n" +
+                "(2,0) |  b  \n" +
+                "(3,0) |  k  \n" +
+                "(4,0) |  q  \n" +
+                "(5,0) |  b  \n" +
+                "(6,0) |  n  \n" +
+                "(7,0) |  null  \n" +
+                "(0,1) |  p  \n" +
+                "(1,1) |  p  \n" +
+                "(2,1) |  p  \n" +
+                "(3,1) |  p  \n" +
+                "(4,1) |  p  \n" +
+                "(5,1) |  p  \n" +
+                "(6,1) |  p  \n" +
+                "(7,1) |  p  \n" +
+                "(0,6) |  p  \n" +
+                "(1,6) |  p  \n" +
+                "(2,6) |  p  \n" +
+                "(3,6) |  p  \n" +
+                "(4,6) |  p  \n" +
+                "(5,6) |  p  \n" +
+                "(6,6) |  p  \n" +
+                "(7,6) |  p  \n" +
+                "(0,7) |  null  \n" +
+                "(1,7) |  n  \n" +
+                "(2,7) |  b  \n" +
+                "(3,7) |  k  \n" +
+                "(4,7) |  q  \n" +
+                "(5,7) |  b  \n" +
+                "(6,7) |  n  \n" +
+                "(7,7) |  null  \n");
     }
 }
